991b5a4be92b54f29d9bf8e4f3960e1b13d602a6
Makoto Kudo <mkudo@machiiro.jp>
2018-05-19 14:40:52 +0900

    build: #570 Rails 5.2 アップデート

--- lib/machiiro/ruby/support/helper/attr_extender.rb
+++ lib/machiiro/ruby/support/helper/attr_extender.rb
@@ -5,6 +5,34 @@ module MachiiroSupport
     end
 
     module ClassMethods
+      def json_fields
+        @json_fields
+      end
+
+      def json_field?(name)
+        @json_fields.include?(name)
+      end
+
+      def attr_json_fields(*names)
+        @json_fields ||= []
+
+        names.each do |name|
+          json_fields.push(name)
+
+          define_method(name) do
+            v = read_attribute(name)
+    
+            if v.is_a?(Hash)
+              return HashWithIndifferentAccess.new(v)
+            elsif v.is_a?(Array)
+              return v.map { |e| e.is_a?(Hash) ? HashWithIndifferentAccess.new(e) : e }
+            end
+    
+            v
+          end
+        end
+      end
+
       def enum_fields
         @enum_fields
       end
