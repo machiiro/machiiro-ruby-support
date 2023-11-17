c22bc259f1c22870c0d2bdaf0bb525f397f76d78
jk-es335 <jun.komori@hacomono.co.jp>
2023-11-17 14:59:18 +0900

    perf: to_h_keyの処理でmapを使わないように修正
    mapを使わずにeach_with_objectを使うように修正し、無駄なループを削減しました

--- spec/machiiro/ruby/support/core_ext/array_spec.rb
+++ spec/machiiro/ruby/support/core_ext/array_spec.rb
@@ -4,5 +4,12 @@ RSpec.describe Array do
     hash = array.to_h_key(:id)
 
     expect(hash.keys).to eq([1, 2, 3])
+    expect(hash.values).to eq([array[0], array[1], array[2]])
+
+    test_class = Struct.new(:id, :value)
+    array = [test_class.new(1, 'test 1'), test_class.new(2, 'test 2'), test_class.new(3, 'test 3')]
+    hash = array.to_h_key(:id)
+    expect(hash.keys).to eq([1, 2, 3])
+    expect(hash.values).to eq([array[0], array[1], array[2]])
   end
 end
