d4a5e951cfbb7e597eea4d9140db74017bdcf9bf
Eiji Iwazawa <eiji.iwazawa@hacomono.co.jp>
2024-04-19 18:33:39 +0900

    build: HCREEN-XXX Bump ruby rails
    - YAML.load_file のデフォルトが safe_load_file に変更されたので、明示的に unsafe_load_file を使えるオプションを追加

--- lib/machiiro/ruby/support/core_ext/yaml.rb
+++ lib/machiiro/ruby/support/core_ext/yaml.rb
@@ -1,9 +1,12 @@
 module YAML
   @cache = {}
 
-  def self.load_file_in_cache(path)
+  # 外部から混入される危険性がないと分かっていて、safe_load_file ではチェックが
+  # 厳しすぎて load できない場合に use_unsafe_load: true を指定できる
+  def self.load_file_in_cache(path, use_unsafe_load: false)
+    load_method = use_unsafe_load ? :unsafe_load_file : :load_file
     yaml = @cache[path]
-    yaml = @cache[path] = YAML.load_file(path) if yaml.nil?
+    yaml = @cache[path] = YAML.send(load_method, path) if yaml.nil?
     yaml
   end
 end
